5f3492ce65505431b0022af51d5ebfcff69ca2c8
MatheusPHB28 <matheus.k.sousa6@aluno.senai.br>
2024-04-11 10:43:58 -0300

    indo

---
+++ condicionais/JS/exercicio2.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro') //true
+}
+else {
+    console.log('Você é Estrangeiro') //false
+}
