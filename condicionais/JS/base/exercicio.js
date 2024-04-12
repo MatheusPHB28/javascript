a18ca6f2ec4cb170d0c96bd83cc825711a2715a9
MatheusPHB28 <matheus.k.sousa6@aluno.senai.br>
2024-04-12 10:49:17 -0300

    indo

---
+++ condicionais/JS/base/exercicio.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A velocdade do seu carro é ${vel} Km/h`)
+if(vel > 60) {
+console.log('Você utrapassou a velocidade permitida,MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ condicionais/JS/multipla/exercicio1.js
@@ -0,0 +1,9 @@
+var hora =8 
+console.log(`Agora são exatamente ${hora}horas `)
+if (horas <12) {
+    console.log('Bom dia')
+}else if (horas <= 18 ) {
+    console.log('Boa tarde')
+}else {
+    console.log('Boa noite')
+}
